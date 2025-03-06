f65a0316dc0b0e9d6f860f4c90c71d2f9b7f2122
polireddy141 <polireddy.veera@gmail.com>
2025-03-06 19:25:31 +0530

    Changed The App.js File

--- iims/src/App.js
+++ iims/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
 import Contact from './components/Contact';
 import Specialists from './components/Specialists';
 import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/specialists" element={<Specialists />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/specialists' element={<Specialists />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
